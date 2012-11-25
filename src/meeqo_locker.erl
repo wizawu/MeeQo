a89f4de918f638ab41036bd1757c29fbf97576ea
wiza <wizawu@gmail.com>
2012-11-25 17:05:46 +0800

    locker added

---
+++ src/meeqo_locker.erl
@@ -0,0 +1,64 @@
+%%
+%%  Copyright (c) 2012 Hualiang Wu <wizawu@gmail.com>
+%%
+%%  Permission is hereby granted, free of charge, to any person obtaining a copy
+%%  of this software and associated documentation files (the "Software"), to
+%%  deal in the Software without restriction, including without limitation the
+%%  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+%%  sell copies of the Software, and to permit persons to whom the Software is
+%%  furnished to do so, subject to the following conditions:
+%%
+%%  The above copyright notice and this permission notice shall be included in
+%%  all copies or substantial portions of the Software.
+%%
+%%  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%%  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+%%  IN THE SOFTWARE.
+%%
+
+-module(meeqo_locker).
+
+-behaviour(gen_server).
+
+-export([start_link/0]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(state, {table}).
+
+%%=============================================================================
+%%  API
+%%=============================================================================
+start_link() ->
+    gen_server:start_link(?MODULE, [], []).
+
+
+%%=============================================================================
+%%  Callback
+%%=============================================================================
+init([]) ->
+    T = ets:new(anonym, [set, public]),
+    {ok, #state{table = T}}.
+
+handle_call(get_key, _From, State) ->
+    {reply, {State#state.table, now()}, State};
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
