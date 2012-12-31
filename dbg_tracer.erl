00dcdfd7e13cd8b96a60a6b3442fe2bd15c412f5
wiza <wizawu@gmail.com>
2013-01-01 01:32:53 +0800

    Amend: dbg_tracer

--- dbg_tracer.erl
+++ dbg_tracer.erl
@@ -6,7 +6,7 @@
 init() ->
     dbg:stop_clear(),
     dbg:tracer(),
-    dbg:p(all, c),
+    dbg:p(new, c),
     dbg:tpl(meeqo, '_', []),
     dbg:tpl(meeqo_inbox, '_', []),
     dbg:tpl(meeqo_sink, '_', []),
@@ -15,7 +15,14 @@ init() ->
     dbg:tpl(meeqo_session, '_', []),
     dbg:tpl(meeqo_pipe, '_', []),
     dbg:tpl(meeqo_msg, '_', []),
+    dbg:tp(gen_server, '_', []),
+    dbg:tp(gen_fsm, '_', []),
     dbg:tp(gen_tcp, '_', []),
     dbg:tp(inet, '_', []),
+    dbg:tp(erlang, put, []),
+    dbg:tp(erlang, erase, []),
+    dbg:tp(ets, insert, []),
+    dbg:tp(ets, delete, []),
+    dbg:tp(ets, new, []),
     ok.
 
